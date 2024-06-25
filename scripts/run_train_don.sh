cf82f4d5b045a4a52c3bdb061eede7ff02a86da6
Jasmine Ratchford <jratchford@sei.cmu.edu>
2024-06-25 05:51:28 +0000

    update run script to ask for working directory

--- scripts/run_train_don.sh
+++ scripts/run_train_don.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
-cd /p/home/jyuko/projects/hspn_surrogate_models
-python3 -m pip install -e .
+
+# Default working directory
+WORKDIR=$(pwd)
+
+# Parse command line options
+while getopts "w:" opt; do
+  case $opt in
+    w) WORKDIR=$OPTARG ;;
+    \?) echo "Invalid option -$OPTARG" >&2 ;;
+  esac
+done
+
+# Change to the specified working directory
+cd $WORKDIR || { echo "Failed to change directory to $WORKDIR"; exit 1; }
+echo "Changed working directory to $WORKDIR"
+
+
+
+#python3 -m pip install --user -e .
 #ls -lh /p/home/jyuko/.local/lib/python3.8/site-packages/
 echo "python3   pyscripts/train_hspn.py  -a configs/don_volume_aoa.yml"
 echo "-----"
